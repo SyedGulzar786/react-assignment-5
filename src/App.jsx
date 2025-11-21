ced0f5547ac793fbbb1bac35d2691c3dd69c4dc3
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-21 03:09:33 -0800

    changing regiter login form ui through state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { BasicForm } from './components';
 import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [type, setType] = useState('login');
+
   return (
     <div className='container'>
       <div className="row justify-content-center p-5">
         <div className="col-6">
-          <h2>Login</h2>
+          <h2>{type === "login" ? "Login" : "Register"}</h2>
           <BasicForm />
-          <Button variant="link">Register Now</Button>
+          <Button onClick={()=>{{type === "login"  ? setType("register") : setType("login")}}} variant="link">{type === "login" ? "Register" : "Login"} Now</Button>
         </div>
       </div>
     </div>
