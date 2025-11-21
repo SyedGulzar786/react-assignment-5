7c63535637a5886ef6c5086640a004826fe6343a
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-21 03:50:52 -0800

    useLayoutEffect, works before ui rendering
    used for tasks need to do before ui rendering

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useLayoutEffect } from "react"
 
 const Counter = ({ count }) => {
     useEffect(() => {
-
         console.log("case 5", count)
 
         return () => {
             console.log("Counter Unmounted")
         }
-
     }, [])
+
+    useEffect(()=>{ // works after ui rendering
+
+    },[])
+
+    useLayoutEffect(()=>{ // works before ui rendering
+
+    },[])
 
     return (
         <h1>{count}</h1>
