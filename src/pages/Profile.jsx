5cb8ee785c6019d6a0efdcf2c4a5167ba2e8e8d0
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-11-24 20:13:31 -0800

    useParams, useLocation , useSearchParams, setSearchParams, searchParams.set()

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,28 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom"
+
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+    const { username } = useParams();
+    const location = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
+    console.log(location, searchParams);
+    console.log(searchParams.get("name"));
+    // console.log(setSearchParams("name"));
+    return (
+        <div>
+            <h1>Profile ({username})</h1>
+            <button onClick={() => {
+                searchParams.set("name", "jilani");
+                setSearchParams(searchParams);
+            }}>
+                update search params 1st way
+            </button>
+            <button onClick={() => {
+                setSearchParams({ name: "gulzar" })
+            }}>
+                update search params 2ndway
+            </button>
+        </div>
+    )
 }
 
 export default Profile
